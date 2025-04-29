0277fba2ccbd0298aacb9d91dc0c14193b37b673
khilola778 <fairyelle18@gmail.com>
2025-04-30 00:53:47 +0500

    Create introsql.sql

---
+++ lesson-1/homework/introsql.sql
@@ -0,0 +1,49 @@
+1. Data: Data refers to raw facts and figures that have no context on their own. It can be numbers,text,images,or sounds that can be proceessed to gain meaningful information.
+2. Database: A database in an organized collection of data that can be easily accessed,managed, and updated. It helps store data in a structured way for efficient retrieval and manipulation.
+3. Relational database: A relational database is a type of database that stores data in tables with rows and columns. It uses relationships between these tables (through keys) to link data logically.
+4. Table: A table is a structure within a relational database that stores data in rows and columns. Each row represents a field or attribute.
+  Five key features of SQL Server:
+ Create
+ Server
+ Alter
+ Drop
+ Delete
+Two different authentication modes available when connecting to SQL Server: 1.Windows Authentification. 2.SQL Server Authentification.
+1. DQL (Data Query Language) – Querying data
+Purpose: Retrieve data from a database.
+Main command:
+Select — fetches data from one or more tables.
+2. DDL (Data Definition Language) – Defining database structure
+Purpose: Create or modify database schema (tables, indexes, etc.).
+Main commands:
+Create — create a new table or object
+Alter— modify an existing object
+Drop— delete an object
+3. DML (Data Manipulation Language) – Manipulating data
+Purpose: Insert, update, or delete data in tables.
+Main commands:
+Insert — add new data
+Update— modify existing data
+Delete— remove data
+4. DCL (Data Control Language) – Access control
+Purpose: Control access and permissions to data.
+Main commands:
+Grant— give access rights
+Revoke— remove access rights
+5. TCL (Transaction Control Language) – Managing transactions
+Purpose: Manage changes made by DML statements.
+Main commands:
+COMMIT — save changes
+Rollback— undo changes
+Begin-transaction
+Differences between SQL Server, SQL and SSMS
+1. SQL (Structured Query Language)
+What it is: A language used to communicate with databases.
+Purpose: Used to create, read, update, and delete (CRUD) data.
+2. SQL Server (Microsoft SQL Server)
+What it is: A relational database management system (RDBMS) developed by Microsoft.
+Purpose: Stores and manages data using SQL.
+Features: Security, data backups, transaction control, stored procedures, and more.
+3. SSMS (SQL Server Management Studio)
+What it is: A graphical user interface (GUI) tool developed by Microsoft.
+Purpose: Allows users to connect to and manage SQL Server databases.
