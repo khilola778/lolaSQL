40d7a698838411dbb8df135dc5b3f46f13bb469d
khilola778 <fairyelle18@gmail.com>
2025-05-02 04:07:27 +0500

    Add files via upload

---
+++ lesson-2/homework/SQLQuery1.sql
@@ -0,0 +1,19 @@
+INSERT INTO employees (EmpID, Name, Salary)
+VALUES (1, 'Alice', 5000.00)
+Insert into employees (EmpID, Name, Salary)
+Values (2, 'Bob', 7000.00)
+INSERT INTO employees (EmpID, Name, Salary)
+values (3, 'Charlie', 5500.00)
+update employees set Salary = 7000 where EmpID = 1
+delete from employees where EmpID = 2
+select * from employees
+alter table employees alter column name VARCHAR(100)
+alter table employees
+add Department Varchar(50)
+alter table employees 
+alter column salary FLOAT
+CREATE TABLE Departments (DepartmentID INT Primary key, DepartmentName varchar(50))
+select * from Departments
+truncate table employees
+
+
