3e281e25690aa0f1bf0c8bef894c8bcc3f737ea4
khilola778 <fairyelle18@gmail.com>
2025-05-02 04:07:01 +0500

    Create intro.sql

---
+++ lesson-2/homework/intro.sql
@@ -0,0 +1,3 @@
+DELETE: Removes specific rows from a table using WHERE. Can be rolled back.
+TRUNCATE: Removes all rows quickly without WHERE. Cannot be rolled back in most DBMS.
+DROP: Deletes the entire table structure and data. Cannot be rolled back.
