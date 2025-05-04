4a2b45d9a90b20b8b32fd97a7deefa37c255703c
khilola778 <fairyelle18@gmail.com>
2025-05-05 01:46:53 +0500

    Create cute.sql

---
+++ lesson-3/homework/cute.sql
@@ -0,0 +1,12 @@
+Bulk Insert — fast import.
+1. CSV (Comma-Separated Values)
+2. TXT (Text File)
+3. XML (eXtensible Markup Language)
+4. JSON (JavaScript Object Notation)
+NULL means no value.
+NOT NULL means value is required
+An IDENTITY column auto-generates unique numbers, usually for primary keys.
+Primary Key: unique, not null, only one per table.
+Unique Key: unique, allows one null, multiple per table.  
+ISNULL: replaces NULL with a value.
+COALESCE: returns first non-NULL from list.
